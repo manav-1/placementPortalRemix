3cce5bc0c6ecf0ed9a41aa7e1f8773513d4f8bd7
Manav Arora <Manav81101@gmail.com>
2023-04-09 00:45:10 +0530

    minor fixes

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,8 +1,15 @@
+import { type LoaderFunction, redirect } from "@remix-run/node";
 import Signup from "~/components/auth/register";
 import { RegisterAction } from "~/utils/auth/actions";
+import { getUser } from "~/utils/auth/auth.server";
 
 export default function Index() {
   return <Signup />;
 }
+
+export const loader: LoaderFunction = async ({ request }) => {
+  // If there's already a user in the session, redirect to the home page
+  return (await getUser(request)) ? redirect("/dashboard/opportunities") : null;
+};
 
 export const action = RegisterAction;
--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,11 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  server: process.env.NODE_ENV === "production" ? "./server.js" : undefined,
-  serverBuildPath: "api/index.js",
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? "./server.js"
+      : undefined,
+  serverBuildPath: ".netlify/functions-internal/server.js",
   future: {
     v2_errorBoundary: true,
     v2_meta: true,
