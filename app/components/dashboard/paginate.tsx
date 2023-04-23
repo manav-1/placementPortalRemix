faf4f5b7c79dead21d879ff01107185082ab9e54
Manav Arora <Manav81101@gmail.com>
2023-04-23 22:29:11 +0530

    added pagination for opportunities & applies opp & conacts

---
+++ app/components/dashboard/paginate.tsx
@@ -0,0 +1,41 @@
+import { Group, Pagination, TextInput } from "@mantine/core";
+import { useLocation, useNavigate } from "@remix-run/react";
+import type { SetStateAction } from "react";
+import { useState } from "react";
+
+export type PaginationProps = {
+  pagination: {
+    page: number;
+    totalPages: number;
+  };
+};
+
+export default function PaginationWithSearch({
+  pagination: { page, totalPages },
+}: PaginationProps) {
+  const navigate = useNavigate();
+  const locaton = useLocation();
+  const [search, setSearch] = useState("");
+
+  const onPageChange = (page: number) => {
+    navigate(`${locaton.pathname}?page=${page}&search=${search}`);
+  };
+  const onSearchChange = (e: { target: { value: SetStateAction<string> } }) => {
+    setSearch(e.target.value);
+    navigate(`${locaton.pathname}?page=${page}&search=${e.target.value}`);
+  };
+  return (
+    <Group>
+      <Pagination value={page} total={totalPages} onChange={onPageChange} />
+      <TextInput
+        value={search}
+        onChange={onSearchChange}
+        placeholder="Search"
+        variant="filled"
+        size="sm"
+        radius="sm"
+        style={{ width: 200 }}
+      />
+    </Group>
+  );
+}
